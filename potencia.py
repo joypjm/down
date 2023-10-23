042356818ec32e175c56bcaece7a0e7b4e2655e4
esteban031 <147660928+esteban031@users.noreply.github.com>
2023-10-23 18:28:58 -0500

    potencia.py

---
+++ potencia.py
@@ -0,0 +1,9 @@
+# Pedimos al usuario que ingrese la base y el exponente
+base = float(input("Ingresa la base: "))
+exponente = int(input("Ingresa el exponente: "))
+
+# Calculamos la potencia
+resultado = base ** exponente
+
+# Mostramos el resultado
+print(f"{base} elevado a la {exponente} es igual a: {resultado}")
