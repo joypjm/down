81775994438bfbabcf5843bdd211c10e23fe7893
esteban031 <147660928+esteban031@users.noreply.github.com>
2023-10-23 18:27:50 -0500

    producto.py

---
+++ producto.py
@@ -0,0 +1,9 @@
+# Pedimos al usuario que ingrese dos números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realizamos la multiplicación
+resultado = numero1 * numero2
+
+# Mostramos el resultado
+print(f"El resultado de la multiplicación es: {resultado}")
